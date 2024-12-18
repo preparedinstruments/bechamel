7d6f8312d0fdfe47306a0d7e365978b463135753
prepared instruments <157577329+preparedinstruments@users.noreply.github.com>
2024-12-19 00:10:22 +0100

    works for horizontal and vertical now

--- verilog/SRAM FPGA PATTERN IN SRAM VGA/verilog.v
+++ verilog/SRAM FPGA PATTERN IN SRAM VGA/verilog.v
@@ -4,8 +4,6 @@ module top(
 clk_in, rec, addr, io, cs, we, oe, key, reset, r_out, g_out, b_out, h_sync, v_sync
 );
 
-wire display_en;
-
 output wire h_sync,
 output wire v_sync,
 
@@ -28,15 +26,9 @@ output reg we;
 output wire oe;
 
 input wire display_en;
-//reg [9:0] h_count;
-input wire [11:0] h_count;
-//reg [9:0] v_count;
-input wire [11:0] v_count;
 
-localparam h_pixel_max = 800; 
-localparam v_pixel_max = 600; 
-localparam h_pixel_half = 400; 
-localparam v_pixel_half = 300;
+input wire [11:0] h_count;
+input wire [11:0] v_count;
 
 wire [7:0] data_in;
 wire [7:0] data_out;
@@ -47,30 +39,10 @@ assign io = (rec==0) ? a : 8'bzzzzzzzz;
 
 assign data_out = b;
 
-// for our bittiness
-reg [11:0] again_xnor;
-
 assign cs = 0; //change this
 assign oe = 0; //change this
 
-
-// to store values from other modules
-wire half_sec;
-
-wire [12:0] random_number_0;
-wire [12:0] random_number_1;
-wire [12:0] random_number_2;
-wire [12:0] random_number_3;
-wire [12:0] random_number_4;
-wire [12:0] random_number_5;
-wire [12:0] random_number_6;
-wire [12:0] random_number_7;
-wire [12:0] random_number_8;
-wire [12:0] random_number_9;
-wire [7:0] sine_wave;
-
-// for a 25MHz clock we divide 100MHz by 4
-reg [1:0] clk_div = 0;
+reg [2:0] clk_div = 0;
 
 always @(posedge clk_in)
 begin
@@ -78,50 +50,39 @@ begin
 end
 
 
-
-assign data_in[7:0] = (h_count[7:0] >= key[7:0]) ? 8'b11111111 : 8'b00000000; // output from FPGA
-
-always @(*) begin
-
-again_xnor[9:0] = (h_count[9:0]^~v_count[9:0]);
-
-end
-
-localparam  h_pixel_total = 1040;
-localparam  h_pixel_display            = 800;
-localparam  h_pixel_front_porch_amount = 56;
-localparam  h_pixel_sync_amount        = 120;
- 
-
-localparam  v_pixel_total              = 666;
-localparam  v_pixel_display            = 600;
-localparam  v_pixel_front_porch_amount = 37;
-localparam  v_pixel_sync_amount        = 6;
-localparam  v_pixel_back_porch_amount  = 23;
+assign data_in[7:0] = (h_count[7:0] + v_count[7:0] >= key[7:0]) ? 8'b11111111 : 8'b00000000; // output from FPGA
 
 
+localparam  v_pixel_display = 600;
 
 //SRAM address counter
 
-always @(posedge clk_div[1]) begin
+always @(posedge clk_div[0]) begin // 50MHz
 
 		if (display_en) begin
-			if( addr < 17'b11111111111111111 && h_count > h_pixel_front_porch_amount && v_count > v_pixel_front_porch_amount && h_count < h_pixel_display  && v_count < v_pixel_display && h_count[0]==1 && v_count[0]==1) begin // basically divide the counters because there are 480K pixels but I have 256K SRAM, and only count the active pixel area
+
+			if(h_count[1] == 1 && v_count[1] == 1)begin
+			
         		addr <= addr+1;
 			end
-			else begin
-			addr <= 0; // I want to restart the counter at the end of each screen draw ?
+			
+			if (v_count >=v_pixel_display || addr >= 18'b111111111111111110 ) begin
+			
+				addr <= 0;
+			
 			end
+			
 		end
-    end
+		
+
+end
 
 //REC control
 
-
-always @(posedge clk_div[1]) begin
+always @(posedge clk_div[0]) begin // 50MHz
       b <= io;
       a <= data_in;
-     if (rec==0) begin
+     if (rec==0 && display_en) begin
           we <= addr[0]; //not sure why it isn't the inverse of addr[0] but that doesn't make the inverse on 'scope
      end
      else begin
@@ -129,31 +90,31 @@ always @(posedge clk_div[1]) begin
      end
 end
 
-always @(posedge clk_in) begin
+always @(posedge clk_div[0]) begin
 			
 	if (display_en) begin
 
-		if ((rec==0) && (a==8'b11111111))
+		if ((rec==0) && (a==8'b11111111) && h_count[0] == 1 && v_count[0] == 1)
 		begin
 					r_out <= 3'b010;
 					g_out <= 3'b010;
 					b_out <= 3'b111;
 		end
-		// display black bar
-		else if ((rec==0) && (a==8'b00000000))
+
+		else if ((rec==0) && (a==8'b00000000)  && h_count[0] == 1 && v_count[0] == 1)
 		begin
 					r_out <= 3'b111;
 					g_out <= 3'b011;
 					b_out <= 3'b110;
 		end
 
-		else if ((rec==1) && (b==8'b11111111)) //data_out not b ??
+		else if ((rec==1) && (b==8'b11111111) ) //data_out not b ??
 		begin
 					r_out <= 3'b011;
 					g_out <= 3'b101;
 					b_out <= 3'b111;
 		end
-		// display black bar
+
 		else
 		begin
 					r_out <= 3'b100;
@@ -174,7 +135,6 @@ vga_sync vga_s(
       .v_count(v_count),
       .display_en(display_en) // '1' => pixel region
       );
-
 
 
 endmodule
