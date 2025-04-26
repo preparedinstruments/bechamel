22d6a65c95bfa4788d77ab51309b205fb970f50a
prepared instruments <157577329+preparedinstruments@users.noreply.github.com>
2025-04-26 10:21:56 +0200

    Add files via upload

---
+++ verilog/SRAM FPGA RPI PERFECTLY RECORDING SINGLE SCREEN SHOT/constraints.v
@@ -0,0 +1,57 @@
+set_io clk 58
+
+set_io rpi_h_sync 62
+set_io rpi_v_sync 61
+set_io rpi_color 52
+
+set_io h_sync 76
+set_io v_sync 97
+
+set_io rec 47
+
+set_io rpi_DEN 63
+
+set_io r_out[0] 91 //for vga
+set_io r_out[1] 95 //for vga
+set_io r_out[2] 96 //for vga
+
+set_io b_out[0] 87 //for vga
+set_io b_out[1] 88 //for vga
+set_io b_out[2] 90 //for vga
+
+set_io g_out[0] 75 //for vga
+set_io g_out[1] 74 //for vga
+set_io g_out[2] 73 //for vga
+
+set_io io[0] 7
+set_io io[1] 8
+set_io io[2] 9
+set_io io[3] 10
+set_io io[4] 11
+set_io io[5] 12
+set_io io[6] 19
+set_io io[7] 22
+
+set_io addr[0] 4
+set_io addr[1] 3
+set_io addr[2] 2
+set_io addr[3] 1
+set_io addr[4] 144
+set_io addr[5] 143
+set_io addr[6] 142
+set_io addr[7] 141
+set_io addr[8] 120
+set_io addr[9] 121
+set_io addr[10] 24
+set_io addr[11] 122
+set_io addr[12] 135
+set_io addr[13] 119
+set_io addr[14] 134
+set_io addr[15] 116
+set_io addr[16] 129
+set_io addr[17] 128
+
+
+set_io cs 25
+set_io oe 23
+set_io we 118
